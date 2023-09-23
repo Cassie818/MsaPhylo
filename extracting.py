0e3e50c54c681b8434b65a7542ebdec158b228db
Ruyi Chen <ruyic818@gmail.com>
2023-09-23 20:08:31 +1000

    implement extracting files

---
+++ extracting.py
@@ -0,0 +1,68 @@
+import torch
+import string
+from esm import Alphabet, MSATransformer, pretrained
+from Bio import SeqIO
+
+
+def remove_insertions(sequence):
+    deletekeys = dict.fromkeys(string.ascii_lowercase)
+    deletekeys["."] = None
+    deletekeys["*"] = None
+    translation = str.maketrans(deletekeys)
+    """ Removes any insertions into the sequence. Needed to load aligned sequences in an MSA. """
+    return sequence.translate(translation)
+
+
+class Extractor:
+
+    def __init__(self, protein_family, msa_type):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model_name = "esm_msa1b_t12_100M_UR50S"
+        self.encoding_dim, self.encoding_layer, self.max_seq_length = 768, 12, 1022
+        self.protein_family = protein_family
+        self.msa_type = msa_type if msa_type in MSA_TYPE_MAP else "default"
+        self.msa_fasta_file = f'{MSA_PATH}{protein_family}{MSA_TYPE_MAP[self.msa_type]}'
+
+    def read_msa(self):
+        return [(record.description, remove_insertions(str(record.seq)))
+                for record in SeqIO.parse(self.msa_fasta_file, "fasta")]
+
+    def get_embedding(self):
+        model, alphabet = pretrained.load_model_and_alphabet(self.model_name)
+        batch_converter = alphabet.get_batch_converter()
+
+        emb = f'{EMB_PATH}{self.protein_family}{EMB_TYPE_MAP[self.msa_type]}{self.model_name}.pt'
+        plm_embedding = {}
+
+        model.eval()
+        msa_data = [self.read_msa()]
+        msa_labels, msa_strs, msa_tokens = batch_converter(msa_data)
+
+        with torch.no_grad():
+            for layer in range(self.encoding_layer):
+                out = model(msa_tokens, repr_layers=[layer], return_contacts=False)
+                token_representations = out["representations"][layer].view(-1, self.sequence_length + 1,
+                                                                           self.encoding_dim)
+                # remove the start token
+                token_representations = token_representations[:, 1:, :]
+                print(f"Finish extracting embeddings from layer {layer}.")
+                plm_embedding[layer] = token_representations
+
+        torch.save(plm_embedding, emb)
+        print("Embeddings saved in output file:", emb)
+
+    def get_col_attention(self):
+        model, alphabet = pretrained.load_model_and_alphabet(self.model_name)
+        batch_converter = alphabet.get_batch_converter()
+
+        attn = f'{ATTN_PATH}{self.protein_family}{ATTN_TYPE_MAP[self.msa_type]}{self.model_name}.pt'
+
+        model.eval()
+        msa_data = [self.read_msa()]
+        msa_labels, msa_strs, msa_tokens = batch_converter(msa_data)
+
+        with torch.no_grad():
+            results = model(msa_tokens, repr_layers=[12], need_head_weights=True)
+
+        torch.save(results, attn)
+        print("Column attention saved in output file:", attn)
---
+++ shuffle data.py
@@ -0,0 +1,117 @@
+import csv
+import random
+from Bio import SeqIO
+
+
+class ChangeAA:
+
+    def __init__(self, protein_family):
+        self.protein_family = protein_family
+        self.msa_file = f'{MSA_PATH}{self.protein_family}_seed_hmmalign_no_inserts.fasta'
+
+    @staticmethod
+    def _shuffle_list(data_list):
+        if len(set(data_list)) == 1:
+            return data_list
+        random.shuffle(data_list)
+        return data_list
+
+    def mix_fasta_column(self):
+        output_file = f'{MSA_PATH}{self.protein_family}_mix_column.fasta'
+        records = list(SeqIO.parse(self.msa_file, "fasta"))
+        seq_length = len(records[0].seq)
+        shuffled_records = []
+
+        for i in range(seq_length):
+            column = ''.join(record.seq[i] for record in records)
+            shuffled_column = self._shuffle_list(list(column))
+            for j, record in enumerate(records):
+                if j >= len(shuffled_records):
+                    shuffled_records.append(record)
+                shuffled_records[j].seq = shuffled_records[j].seq[:i] + shuffled_column[j] + shuffled_records[j].seq[i+1:]
+
+        SeqIO.write(shuffled_records, output_file, "fasta")
+        print('Generated Mix columns data!')
+
+    def _read_fasta(self):
+        with open(self.msa_file, 'r') as file:
+            lines = file.readlines()
+
+        sequences = []
+        current_sequence = ''
+        for line in lines:
+            line = line.rstrip()
+            if line.startswith('>'):
+                if current_sequence:
+                    sequences.append(current_sequence)
+                sequences.append(line)
+                current_sequence = ''
+            else:
+                current_sequence += line
+        sequences.append(current_sequence)
+
+        return sequences
+
+    def shuffle_fasta_all(self):
+        output_seq_file = f'{MSA_PATH}{self.protein_family}_shuffle_all.fasta'
+        output_order_file = f'{MSA_PATH}{self.protein_family}_shuffle_all_order.txt'
+        sequences = self._read_fasta()
+        sequence_length = len(sequences[1])
+
+        shuffled_sequences = []
+        shuffled_order = []
+
+        for sequence in sequences:
+            if not sequence.startswith('>'):
+                shuffled_indices = random.sample(range(sequence_length), sequence_length)
+                shuffled_order.append(shuffled_indices)
+                shuffled_sequence = ''.join(sequence[i] for i in shuffled_indices)
+                shuffled_sequences.append(shuffled_sequence)
+            else:
+                shuffled_sequences.append(sequence)
+
+        with open(output_seq_file, 'w') as file:
+            file.write('\n'.join(shuffled_sequences))
+
+        with open(output_order_file, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(shuffled_order)
+
+        print('Generated Shuffle all data!')
+
+    def shuffle_fasta_column(self):
+        output_seq_file = f'{MSA_PATH}{self.protein_family}_shuffle_column.fasta'
+        output_order_file = f'{MSA_PATH}{self.protein_family}_shuffle_column_order.txt'
+        sequences = self._read_fasta()
+        sequence_length = len(sequences[1])
+
+        shuffled_order = [random.sample(range(sequence_length), sequence_length)]
+        shuffled_sequences = []
+
+        for sequence in sequences:
+            if not sequence.startswith('>'):
+                shuffled_sequence = ''.join(sequence[i] for i in shuffled_order[0])
+                shuffled_sequences.append(shuffled_sequence)
+            else:
+                shuffled_sequences.append(sequence)
+
+        with open(output_seq_file, 'w') as file:
+            file.write('\n'.join(shuffled_sequences))
+
+        with open(output_order_file, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(shuffled_order)
+
+        print('Generated Shuffle columns data!')
+
+
+if __name__ == '__main__':
+    MSA_PATH = './Msa/'
+    MSA_TYPE_MAP = {
+        "default": "_seed_hmmalign_no_inserts.fasta",
+        "sc": "_shuffle_column.fasta",
+        "sa": "_shuffle_all.fasta",
+        "mc": "_mix_column.fasta"
+    }
+
+
