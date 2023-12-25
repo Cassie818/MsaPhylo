3c1cd0a00a446019f02092ef0c1b55fffef8fd44
Ruyi Chen <ruyic818@gmail.com>
2023-12-25 20:50:21 +0800

    modify calc_pfam_stats.py

--- code/calc_pfam_stats.py
+++ code/calc_pfam_stats.py
@@ -2,69 +2,60 @@ import os
 import pandas as pd
 from Bio import SeqIO
 from ete3 import Tree
-
-data_dict = {}
-current_ac = None
+import glob
 
 
-def calc_stats(alignments_folder, trees_folder, output_file):
-    # Create an empty DataFrame to store the statistics
+def get_alignment_stats(alignments_file):
+    sequences = list(SeqIO.parse(alignments_file, "fasta"))
+    sequence_lengths = [len(seq.seq) for seq in sequences]
+    no_gap_lengths = [len(str(seq.seq).replace("-", "")) for seq in sequences]
+    num_sequences = len(sequences)
+    alignment_length = sequence_lengths[0] if sequence_lengths else 0
+    no_gap_length_avg = sum(no_gap_lengths) / num_sequences if num_sequences else 0
+    return num_sequences, alignment_length, no_gap_length_avg
+
+
+def get_tree_height_avg(tree_file):
+    tree = Tree(tree_file)
+    tree_heights = [leaf.dist for leaf in tree.get_leaves()]
+    average_height = sum(tree_heights) / len(tree_heights) if tree_heights else 0
+    return average_height
+
+
+def calc_stats(alignments_folder, trees_folder1, trees_folder2, output_file):
     data = []
 
-    # Iterate over alignment files
-    for filename in os.listdir(alignments_folder):
+    for alignments_file in glob.glob(os.path.join(alignments_folder, "*.fasta")):
+        base_name = os.path.splitext(os.path.basename(alignments_file))[0]
+        trees_file1 = os.path.join(trees_folder1, base_name + ".tree")
+        trees_file2 = os.path.join(trees_folder2, base_name + ".tree")
 
-        alignments_file = os.path.join(alignments_folder, filename)
-        trees_file = os.path.join(trees_folder, filename.split(".")[0] + ".tree")
+        if os.path.isfile(trees_file1):
+            print("Calculating statistics for:", base_name)
 
-        if os.path.isfile(alignments_file) and os.path.isfile(trees_file):
-            print("Calculating statistics for:", filename)
+            num_sequences, alignment_length, no_gap_length_avg = get_alignment_stats(alignments_file)
+            avg_tree_height_nj = get_tree_height_avg(trees_file1)
+            avg_tree_height_ml = get_tree_height_avg(trees_file2)
 
-            # Calculate sequence length and number of sequences
-            sequences = SeqIO.parse(alignments_file, "fasta")
-            sequence_lengths = []
-            no_gap_lengths = []
-            num_sequences = 0
+            data.append(
+                [base_name, num_sequences, alignment_length, no_gap_length_avg, avg_tree_height_nj, avg_tree_height_ml])
 
-            for sequence in sequences:
-                sequence_lengths.append(len(sequence.seq))
-                no_gap_lengths.append(len(str(sequence.seq).replace(".", "")))
-                num_sequences += 1
-
-            # Calculate alignment length
-            alignment_length = sequence_lengths[0]  # Length of the first sequence
-
-            no_gap_lengths_average = sum(no_gap_lengths) / num_sequences
-            # Calculate average height of the tree
-            tree = Tree(trees_file)
-            tree_heights = [leaf.dist for leaf in tree.get_leaves()]
-            average_height = sum(tree_heights) / len(tree_heights)
-
-            # Add the statistics to the DataFrame
-            data.append([filename.split(".")[0], num_sequences, alignment_length,
-                         no_gap_lengths_average, average_height])
-
-    # Create a DataFrame from the collected data
-    df = pd.DataFrame(data, columns=["pfam_id", "sequence_num", "alignment_len", "avg_sequence_len",
-                                     "average_tree_height"])
-
-    # Write the DataFrame to a TSV file
+    df = pd.DataFrame(data,
+                      columns=["pfam_id", "sequence_num", "alignment_len", "avg_sequence_len", "average_tree_height_NJ",
+                               "average_tree_height_ML"])
     df.to_csv(output_file, sep='\t', index=False)
 
 
 # Get the current working directory
 cwd = os.getcwd()
-
 # Set the alignments and trees folders as subdirectories of the current working directory
-alignments_folder = os.path.join(cwd, "alignments")
-trees_folder = os.path.join(cwd, "trees")
-# pfam_file_path = os.path.join(cwd, "unprocessed_pfam_data/example.pfam")
-pfam_file_path = os.path.join(cwd, "unprocessed_pfam_data/Pfam-A.seed")
+alignments_folder = os.path.join(cwd, "data/Pfam")
+trees_folder1 = os.path.join(cwd, "Trees/Pfam/NJ")
+trees_folder2 = os.path.join(cwd, "Trees/Pfam/ML")
 
 # Specify the output file path
 output_file = "pfam_summary.tsv"
 
 # Call the function to calculate statistics for all alignment files and write them to the output file
-calc_stats(alignments_folder, trees_folder, output_file)
-
+calc_stats(alignments_folder, trees_folder1, trees_folder2, output_file)
 print("Statistics have been written to", output_file)
