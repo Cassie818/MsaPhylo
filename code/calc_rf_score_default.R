7b8788849ea7c86bf7f7ae7d2d33f8fbb70f8d8f
Ruyi Chen <ruyic818@gmail.com>
2024-01-15 22:52:44 +0800

    update calculate_simi_score.R

--- code/calc_rf_score_default.R
+++ code/calc_rf_score_default.R
@@ -31,43 +31,46 @@ write_scores <- function(data, file_path, file_exists) {
 for (prot_domain in prot_domains) {
   pattern_string <- paste0("^", prot_domain, ".*\\.nwk$")
   emb_files <- list.files(path = emb_folder, pattern = pattern_string, full.names = TRUE)
-
+  
   # Construct paths for NJ and ML trees
   njtree <- file.path(nj_tree_folder, paste0(prot_domain, ".tree"))
   mltree <- file.path(ml_tree_folder, paste0(prot_domain, ".tree"))
-
+  
   # Read the NJ and ML trees
   nj_tree <- ape::read.tree(njtree)
   ml_tree <- ape::read.tree(mltree)
-
+  
   # Loop through each embedding file
   for (emb_file in emb_files) {
     emb_tree <- ape::read.tree(emb_file)
-
+    
     # Calculate Robinson Foulds scores
     rf_nj_score <- RobinsonFoulds(emb_tree, nj_tree, similarity = TRUE, normalize = TRUE)
     rf_ml_score <- RobinsonFoulds(emb_tree, ml_tree, similarity = TRUE, normalize = TRUE)
-
+    
     file_name <- basename(sub("\\.nwk$", "", emb_file))
     nj_data <- data.frame(FileName = file_name, RFScore = rf_nj_score)
     ml_data <- data.frame(FileName = file_name, RFScore = rf_ml_score)
-
+    
     # Write NJ and ML scores to respective files
     write_scores(nj_data, nj_file_path, nj_file_exists)
     write_scores(ml_data, ml_file_path, ml_file_exists)
-
+    
     # Update file existence status
     nj_file_exists <- TRUE
     ml_file_exists <- TRUE
   }
-
+  
   # Calculate and record the Robinson Foulds score between NJ and ML trees
   domain_name <- basename(prot_domain)
   rf_nj_ml_score <- RobinsonFoulds(nj_tree, ml_tree, similarity = TRUE, normalize = TRUE)
   nj_ml_data <- data.frame(ProteinDomain = domain_name, RFScore = rf_nj_ml_score)
-
+  
   write_scores(nj_ml_data, nj_ml_file_path, nj_ml_file_exists)
   nj_ml_file_exists <- TRUE  # Update file existence status
 }
+
+
+
 
 
