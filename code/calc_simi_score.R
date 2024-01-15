6b615eabbfb6a2aa110b5131c73a6e439069e01a
Ruyi Chen <ruyic818@gmail.com>
2024-01-15 21:31:31 +0800

    update calculate_simi_score.R

--- code/calc_simi_score.R
+++ code/calc_simi_score.R
@@ -1,51 +1,73 @@
-# Load required library
+# Load required libraries
 library("TreeDist")
 library("ape")
 
-# Set the directory where the NJtree files are located
+# Set the directory paths
 emb_folder <- "/Users/cassie/Desktop/PlmPlylo/embeddings"
+nj_tree_folder <- "/Users/cassie/Desktop/MsaPhylo/Trees/Pfam/NJ/"
+ml_tree_folder <- "/Users/cassie/Desktop/MsaPhylo/Trees/Pfam/ML/"
 prot_domains <- c('PF00066','PF00168','PF00484','PF00672',
-                 'PF00699','PF01951','PF03147','PF03463',
-                 'PF04972','PF06427','PF07498','PF07677',
-                 'PF10114','PF10576','PF12172','PF12872',
-                 'PF13377','PF13466','PF14317','PF20171')
+                  'PF00699','PF01951','PF03147','PF03463',
+                  'PF04972','PF06427','PF07498','PF07677',
+                  'PF10114','PF10576','PF12172','PF12872',
+                  'PF13377','PF13466','PF14317','PF20171')
 
+# File paths for output
+nj_file_path <- "/Users/cassie/Desktop/PlmPlylo/embeddings/nj_rf_score.csv"
+ml_file_path <- "/Users/cassie/Desktop/PlmPlylo/embeddings/ml_rf_score.csv"
+nj_ml_file_path <- "/Users/cassie/Desktop/PlmPlylo/embeddings/nj_ml_rf_score.csv"
 
-# Get a list of all files in the directory with extension ".nwk"
+# Check if files exist to determine if column names should be written
+nj_file_exists <- file.exists(nj_file_path)
+ml_file_exists <- file.exists(ml_file_path)
+nj_ml_file_exists <- file.exists(nj_ml_file_path)
+
+# Function to write scores to files
+write_scores <- function(data, file_path, file_exists) {
+  write.table(data, file = file_path, append = TRUE, sep = ",", col.names = !file_exists, row.names = FALSE)
+}
+
+# Loop through each protein domain
 for (prot_domain in prot_domains) {
   pattern_string <- paste0("^", prot_domain, ".*\\.nwk$")
   emb_files <- list.files(path = emb_folder, pattern = pattern_string, full.names = TRUE)
-  # Load the NJ tree for this protein domain
-  njtree <- paste0("/Users/cassie/Desktop/MsaPhylo/Trees/Pfam/NJ/", prot_domain, ".tree")
+
+  # Construct paths for NJ and ML trees
+  njtree <- file.path(nj_tree_folder, paste0(prot_domain, ".tree"))
+  mltree <- file.path(ml_tree_folder, paste0(prot_domain, ".tree"))
+
+  # Read the NJ and ML trees
   nj_tree <- ape::read.tree(njtree)
-  # Load the ML tree for this protein domain
-  mltree <- paste0("/Users/cassie/Desktop/MsaPhylo/Trees/Pfam/ML/", prot_domain, ".tree")
   ml_tree <- ape::read.tree(mltree)
 
-  # Create an empty list to store the scores
-  rf_score_nj_list <- list()
-  rf_score_ml_list <- list()
-
-  # Loop through each njtree file and calculate the score
+  # Loop through each embedding file
   for (emb_file in emb_files) {
-
-    # Read the embedding trees
     emb_tree <- ape::read.tree(emb_file)
 
-    # Calculate the Robinson Foulds score
+    # Calculate Robinson Foulds scores
     rf_nj_score <- RobinsonFoulds(emb_tree, nj_tree, similarity = TRUE, normalize = TRUE)
     rf_ml_score <- RobinsonFoulds(emb_tree, ml_tree, similarity = TRUE, normalize = TRUE)
 
-    # Store the score in the list with the corresponding file name
     file_name <- basename(sub("\\.nwk$", "", emb_file))
-    nj_data <- data.frame(Column1 = file_name, Column2 = rf_nj_score)
-    ml_data <- data.frame(Column1 = file_name, Column2 = rf_ml_score)
+    nj_data <- data.frame(FileName = file_name, RFScore = rf_nj_score)
+    ml_data <- data.frame(FileName = file_name, RFScore = rf_ml_score)
 
-    nj_file_path <- "/Users/cassie/Desktop/PlmPlylo/embeddings/nj_rf_score.csv"
-    ml_file_path <- "/Users/cassie/Desktop/PlmPlylo/embeddings/ml_rf_score.csv"
-    write.table(nj_data, file = nj_file_path, append = TRUE, sep = ",", col.names = !file.exists(file_path), row.names = FALSE)
-    write.table(ml_data, file = ml_file_path, append = TRUE, sep = ",", col.names = !file.exists(file_path), row.names = FALSE)
+    # Write NJ and ML scores to respective files
+    write_scores(nj_data, nj_file_path, nj_file_exists)
+    write_scores(ml_data, ml_file_path, ml_file_exists)
 
+    # Update file existence status
+    nj_file_exists <- TRUE
+    ml_file_exists <- TRUE
   }
+
+  # Calculate and record the Robinson Foulds score between NJ and ML trees
+  domain_name <- basename(prot_domain)
+  rf_nj_ml_score <- RobinsonFoulds(nj_tree, ml_tree, similarity = TRUE, normalize = TRUE)
+  nj_ml_data <- data.frame(ProteinDomain = domain_name, RFScore = rf_nj_ml_score)
+
+  write_scores(nj_ml_data, nj_ml_file_path, nj_ml_file_exists)
+  nj_ml_file_exists <- TRUE  # Update file existence status
 }
+
 
