455ceba352035321f5061379a45b8f3a3d98b65e
Ruyi Chen <ruyic818@gmail.com>
2024-01-19 17:11:08 +1000

    modify rfscore_attn.py

--- Plot/rfscore_attn.py
+++ Plot/rfscore_attn.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import os
 import pandas as pd
+import numpy as np
 
 
 def load_data(typ):
@@ -85,37 +86,83 @@ def load_data(typ):
     return final_default, final_sc, final_scovar
 
 
-def plot_protein_domains(default_df, sc_df, scovar_df, prot_domains):
-    fig, axs = plt.subplots(4, 3, figsize=(9, 12),
-                            gridspec_kw={"width_ratios": [10, 10, 10]},
-                            constrained_layout=True)
+def extract_data(df, protein_domain):
+    data = df[df['ProteinDomain'] == protein_domain]
+    return data['RFScore'].to_numpy().reshape(12, 12)
 
+
+def extract_var(df, protein_domain):
+    var = df[df['ProteinDomain'] == protein_domain]
+    return var['Variance'].to_numpy().reshape(12, 12)
+
+
+def plot_heatmap(ax, data, vmax):
+    im = ax.imshow(data, cmap=red_white_cmap, aspect='equal', vmin=0, vmax=vmax)
+    ax.set_xticks(np.arange(0, 12, 2))
+    ax.set_yticks(np.arange(0, 12, 2))
+    ax.set_xticklabels(x_labels[::2])
+    ax.set_yticklabels(y_labels[::2])
+    return im
+
+
+def overlay_variance(var_data, ax):
+    max_variance = np.max(var_data)
+    for i in range(var_data.shape[0]):
+        for j in range(var_data.shape[1]):
+            normalized_var = var_data[i, j] / max_variance
+            circle_size = normalized_var * 100
+            ax.scatter(j, i, s=circle_size, color='gray', alpha=0.5)
+
+
+def plot_protein_domains(default_df, sc_df, scovar_df):
+    # fig, axs = plt.subplots()
+    fig, axs = plt.subplots(4, 3, figsize=(9, 12), gridspec_kw={"width_ratios": [10, 10, 10]})
+
+    for i, protein_domain in enumerate(prot_domains):
+
+        default = extract_data(default_df, protein_domain)
+        sc = extract_data(sc_df, protein_domain)
+        scovar = extract_data(scovar_df, protein_domain)
+        vmax = np.max([default, sc, scovar])
+
+        var_of_sc = extract_var(sc_df, protein_domain)
+        var_of_scovar = extract_var(scovar_df, protein_domain)
+
+        for j, typ in enumerate(typs):
+            if typ == 'Default':
+                heatmap = plot_heatmap(axs[i, j], default, vmax)
+            elif typ == 'Shuffled_columns':
+                heatmap = plot_heatmap(axs[i, j], sc, vmax)
+            else:
+                heatmap = plot_heatmap(axs[i, j], scovar, vmax)
+
+            if i == 3:
+                axs[i, j].set_xlabel("Heads", fontsize=12)
+            if i == 0:
+                axs[0, j].set_title(typ, fontsize=12)
+            if j == 0:
+                axs[i, j].set_ylabel(f"{protein_domain}\nLayers", fontsize=12)
+            if j == 2:
+                fig.colorbar(heatmap, ax=axs[i, j])
+            if j != 0:
+                overlay_variance(var_of_sc, axs[i, j])
+                overlay_variance(var_of_scovar, axs[i, j])
+
+    plt.subplots_adjust(wspace=0.01, hspace=0.35)
+    plt.show()
+
+
+if __name__ == '__main__':
+    prot_domains = ['PF00066', 'PF00168', 'PF00484', 'PF00672']
+    typs = ['Default', 'Shuffled_columns', 'Shuffled_covariance']
+    x_labels = [str(i) for i in range(1, 13)]
+    y_labels = [str(i) for i in range(1, 13)]
     cdict = {'red': [(0.0, 1.0, 1.0),
                      (1.0, 1.0, 1.0)],
              'green': [(0.0, 1.0, 1.0),
                        (1.0, 0.0, 0.0)],
              'blue': [(0.0, 1.0, 1.0),
                       (1.0, 0.0, 0.0)]}
-
     red_white_cmap = mcolors.LinearSegmentedColormap('RedWhite', cdict)
-
-    for i, protein_domain in enumerate(prot_domains):
-        default_data = default_df[default_df['ProteinDomain'] == protein_domain]
-        sc_data = sc_df[sc_df['ProteinDomain'] == protein_domain]
-        scovar_data = scovar_df[scovar_df['ProteinDomain'] == protein_domain]
-        default = default_data['RFScore'].to_numpy().reshape(12, 12)
-        sc = sc_data['Mean'].to_numpy().reshape(12, 12)
-        scovar = scovar_data['Mean'].to_numpy().reshape(12, 12)
-        axs[i, 0].imshow(default, cmap=red_white_cmap, aspect='equal', vmin=0, vmax=0.5)
-        axs[i, 1].imshow(sc, cmap=red_white_cmap, aspect='equal', vmin=0, vmax=0.5)
-        axs[i, 2].imshow(scovar, cmap=red_white_cmap, aspect='equal', vmin=0, vmax=0.5)
-
-    plt.subplots_adjust(hspace=0.3, wspace=0.2)
-    plt.tight_layout(rect=[0, 0.03, 1, 0.97])
-    plt.show()
-
-
-if __name__ == '__main__':
-    prot_domains = ['PF00066', 'PF00168', 'PF00484', 'PF00672']
     final_default, final_sc, final_scovar = load_data('ml')
     plot_protein_domains(final_default, final_sc, final_scovar, prot_domains)
