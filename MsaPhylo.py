19d03062d75567a7e181387fda3e9bc25a4b7049
Ruyi Chen <ruyic818@gmail.com>
2024-03-02 16:13:44 +1000

    add MsaPhylo.py

--- MsaPhylo.py
+++ MsaPhylo.py
@@ -1,4 +1,13 @@
 import argparse
 
+
+def main():
+    # Parse command-line arguments.
+    parser = argparse.ArgumentParser(description='Building the phylogenetic trees using the MSA Transformer.')
+    parser.add_argument('--input', required=True, help='Input FASTA file path')
+    parser.add_argument('--output', required=True, help='Output path to save the phylogenetic trees')
+    parser.add_argument('--layer', required=False, help='Specify the layer of the MSA Transformer')
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    main()
