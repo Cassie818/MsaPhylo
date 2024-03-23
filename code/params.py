cb9314bafd5ac62e5a2043b9cefb21db882b67ea
Ruyi Chen <ruyic818@gmail.com>
2024-03-24 09:19:19 +1000

    modify para path

--- code/params.py
+++ code/params.py
@@ -8,19 +8,22 @@ TREE_PATH = './Trees/'
 MSA_TYPE_MAP = {
     "default": ".fasta",
     "sc": "_shuffle_columns.fasta",
-    "scovar": "_shuffle_covariance.fasta"
+    "scovar": "_shuffle_covariance.fasta",
+    "sr": "_shuffle_rows.fasta"
 }
 
 EMB_TYPE_MAP = {
     "default": "_emb_",
     "sc": "_emb_shuffle_columns_",
-    "scovar": "_emb_shuffle_covariance_"
+    "scovar": "_emb_shuffle_covariance_",
+    "sr": "_emb_shuffle_rows_"
 }
 
 ATTN_TYPE_MAP = {
     "default": "_attn_",
     "sc": "_attn_shuffle_columns_",
-    "scovar": "_attn_shuffle_covariance_"
+    "scovar": "_attn_shuffle_covariance_",
+    "sr": "_attn_shuffle_rows_"
 }
 
 LAYER = 12
