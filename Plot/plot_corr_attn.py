b545d075756bed9662a05ad5fe2bc5162cba2aca
Ruyi Chen <ruyic818@gmail.com>
2024-03-24 10:58:13 +1000

    modify paras

--- Plot/plot_corr_attn.py
+++ Plot/plot_corr_attn.py
@@ -2,13 +2,13 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import List, Dict
 
 
 def load_data(domain_name: str):
     default_dict = {}
     sc_dict = {}
     scovar_dict = {}
+    sr_dict = {}
     base_path = 'Results'
 
     default_corr_file = os.path.join(base_path, f'{domain_name}_default_ev_analysis_col_attention.csv')
@@ -30,10 +30,15 @@ def load_data(domain_name: str):
                 scovar = pd.read_csv(scovar_file)["Correlation"].to_numpy().reshape(12, 12)
                 scovar_dict[f'{dir}_{domain_name}_Shuffled_covariance'] = scovar
 
-    return default_dict, sc_dict, scovar_dict
+            sr_file = os.path.join(dir_path, f'{domain_name}_sr_ev_analysis_col_attention.csv')
+            if os.path.exists(sr_file):
+                sr = pd.read_csv(sr_file)["Correlation"].to_numpy().reshape(12, 12)
+                sr_dict[f'{dir}_{domain_name}_Shuffled_rows'] = sr
+
+    return default_dict, sc_dict, scovar_dict, sr_dict
 
 
-def calculate_mean_variance(domain_dict: Dict):
+def calculate_mean_variance(domain_dict):
     first_key = next(iter(domain_dict))
     rows, cols = domain_dict[first_key].shape
 
@@ -49,7 +54,7 @@ def calculate_mean_variance(domain_dict: Dict):
     return mean_matrix, variance_matrix
 
 
-def load_domain_abs_vmax(domain_name: str, data_dict: Dict):
+def load_domain_abs_vmax(domain_name, data_dict):
     all_data = []
 
     for data in data_dict[domain_name].values():
@@ -61,22 +66,24 @@ def load_domain_abs_vmax(domain_name: str, data_dict: Dict):
     return domain_abs_vmax
 
 
-def create_domain_heatmaps(mean_dict: Dict, var_dict: Dict, protein_domains: List[int],
-                           domain_abs_max: Dict, num_protein=4):
+def create_domain_heatmaps(mean_dict, var_dict, protein_domains, domain_abs_max, num_protein=4):
     """
     Create and display heatmaps for protein domains with variance overlay.
+
     :param mean_dict: Dictionary containing the mean heatmap data for each protein domain.
     :param var_dict: Dictionary containing the variance data for each protein domain.
     :param protein_domains: List of protein domain names.
     :param domain_abs_max: Dictionary of maximum absolute value for each domain.
     :param num_protein: Number of proteins (default is 4).
     """
-    typs = ['Default', 'Shuffled_columns', 'Shuffled_covariance']
+    typs = ['Default', 'Shuffled_columns', 'Shuffled_covariance', 'Shuffled_rows']
     x_labels = [str(i) for i in range(1, 13)]
     y_labels = [str(i) for i in range(1, 13)]
 
-    fig, axes = plt.subplots(nrows=num_protein, ncols=len(typs), figsize=(9, 12),
-                             gridspec_kw={"width_ratios": [10, 10, 12]})
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['font.sans-serif'] = ['arial']
+    fig, axes = plt.subplots(nrows=num_protein, ncols=len(typs), figsize=(10, 10),
+                             gridspec_kw={"width_ratios": [10, 10, 10, 13]}, )
 
     for i, protein_domain in enumerate(protein_domains):
         pf_info_mean = mean_dict[protein_domain]
@@ -96,14 +103,16 @@ def create_domain_heatmaps(mean_dict: Dict, var_dict: Dict, protein_domains: Lis
                 axes[i, j].set_xlabel("Head", fontsize=12)
             if j != 0:
                 overlay_variance(var_data, axes[i, j])
-            if j == 2:
+            if j == 3:
                 fig.colorbar(heatmap, ax=axes[i, j])
 
     # Set column titles
-    for j, typ in enumerate(typs):
-        axes[0, j].set_title(typ.replace("_", " "), fontsize=12)
+    axes[0, 0].set_title("Default", fontsize=12)
+    axes[0, 1].set_title("Shuffled Positions", fontsize=12)
+    axes[0, 2].set_title("Shuffled Covariance", fontsize=12)
+    axes[0, 3].set_title("Shuffled Rows", fontsize=12)
 
-    plt.subplots_adjust(wspace=0.01, hspace=0.35)
+    plt.subplots_adjust(wspace=0.15, hspace=0.15)
     plt.show()
 
 
@@ -130,15 +139,19 @@ if __name__ == '__main__':
     var_dict = {}
     domain_abs_vmax = {}
 
-    protein_domains = ['PF00066', 'PF00168', 'PF00484', 'PF00672']
+    protein_domains = ['PF13377', 'PF13466', 'PF14317', 'PF20171']
     for domain in protein_domains:
-        default_dict, sc_dict, scovar_dict = load_data(domain)
+        default_dict, sc_dict, scovar_dict, sr_dict = load_data(domain)
         mean_matrix_sc, variance_matrix_sc = calculate_mean_variance(sc_dict)
         mean_matrix_scovar, variance_matrix_scovar = calculate_mean_variance(scovar_dict)
+        mean_matrix_sr, variance_matrix_sr = calculate_mean_variance(sr_dict)
+
         mean_dict[f'{domain}'] = {f'{domain}_Default': default_dict[f'{domain}_Default'],
                                   f'{domain}_Shuffled_columns': mean_matrix_sc,
-                                  f'{domain}_Shuffled_covariance': mean_matrix_scovar}
+                                  f'{domain}_Shuffled_covariance': mean_matrix_scovar,
+                                  f'{domain}_Shuffled_rows': mean_matrix_sr}
         var_dict[f'{domain}'] = {f'{domain}_Shuffled_columns': variance_matrix_sc,
-                                 f'{domain}_Shuffled_covariance': variance_matrix_scovar}
+                                 f'{domain}_Shuffled_covariance': variance_matrix_scovar,
+                                 f'{domain}_Shuffled_rows': variance_matrix_sr}
         domain_abs_vmax[domain] = load_domain_abs_vmax(domain, mean_dict)
     create_domain_heatmaps(mean_dict, var_dict, protein_domains, domain_abs_vmax)
