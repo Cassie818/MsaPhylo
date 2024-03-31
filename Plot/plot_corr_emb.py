03678984e03f7ec6b924e3a80a8e5cdf6dc6381c
Ruyi Chen <ruyic818@gmail.com>
2024-03-31 21:34:59 +1000

    modify paras

--- Plot/plot_corr_emb.py
+++ Plot/plot_corr_emb.py
@@ -2,34 +2,45 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import List, Dict
 
 
-def load_correlation_data(base_path: str, domain_name: str, analysis_type: str):
-    """
-    Load correlation data for a given analysis type.
-    """
-    data_dict = {}
-    file_suffix = f"{domain_name}_{analysis_type}_ev_and_euclidean_analysis_emb.csv"
+def load_data(domain_name):
+    base_path = 'Results'
 
-    if analysis_type == 'default':
-        file_path = os.path.join(base_path, file_suffix)
-        if os.path.exists(file_path):
-            data_dict['default'] = pd.read_csv(file_path)["Correlation"].to_list()
+    # load default data
+    default_corr_file = os.path.join(base_path, f'{domain_name}_default_ev_and_euclidean_analysis_emb.csv')
+    if os.path.exists(default_corr_file):
+        default_corr = pd.read_csv(default_corr_file)["Correlation"].to_list()
     else:
-        for root, dirs, files in os.walk(base_path):
-            for dir in dirs:
-                file_path = os.path.join(root, dir, file_suffix)
-                if os.path.exists(file_path):
-                    data_dict[dir] = pd.read_csv(file_path)["Correlation"].to_list()
+        default_corr = []
 
-    return data_dict
+    # load shuffle data
+    sc_dict = {}
+    scovar_dict = {}
+    sr_dict = {}
+
+    for root, dirs, files in os.walk(base_path):
+        for dir in dirs:
+            sc_file_path = os.path.join(root, dir, f'{domain_name}_sc_ev_and_euclidean_analysis_emb.csv')
+            scovar_file_path = os.path.join(root, dir, f'{domain_name}_scovar_ev_and_euclidean_analysis_emb.csv')
+            sr_file_path = os.path.join(root, dir, f'{domain_name}_sr_ev_and_euclidean_analysis_emb.csv')
+
+            if os.path.exists(sc_file_path):
+                sc_data = pd.read_csv(sc_file_path)["Correlation"].to_list()
+                sc_dict[dir] = sc_data
+
+            if os.path.exists(scovar_file_path):
+                scovar_data = pd.read_csv(scovar_file_path)["Correlation"].to_list()
+                scovar_dict[dir] = scovar_data
+
+            if os.path.exists(sr_file_path):
+                sr_data = pd.read_csv(sr_file_path)["Correlation"].to_list()
+                sr_dict[dir] = sr_data
+
+    return default_corr, sc_dict, scovar_dict, sr_dict
 
 
-def calculate_stats(data_dict: Dict):
-    """
-    Calculate mean and standard deviation for data in data_dict.
-    """
+def calculate_stats(data_dict):
     list_length = len(next(iter(data_dict.values())))
     layer_data = {i: [data_dict[key][i] for key in data_dict] for i in range(list_length)}
     means = [np.mean(values) for values in layer_data.values()]
@@ -37,62 +48,55 @@ def calculate_stats(data_dict: Dict):
     return means, stds
 
 
-def plot_correlations(protein_domains: List, base_path: str, reference_csv: str):
-    """
-    Plot correlation data for multiple protein domains.
-    """
-    ref = pd.read_csv(reference_csv)
-    ref_dict = ref.set_index('ProteinDomain')['Corr'].to_dict()
+with open('./data/Pfam/protein_domain.txt', 'r') as file:
+    lines = file.readlines()
+    protein_domains = [line.strip() for line in lines]
 
-    plt.rcParams['font.family'] = ['DejaVu Sans']
-    fig, axes = plt.subplots(4, 5, figsize=(12, 8))
-    axes = axes.flatten()
+# plt.style.use('seaborn-whitegrid')
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = ['arial']
+fig, axes = plt.subplots(4, 5, figsize=(12, 8))
+ref = pd.read_csv('score/nj_ml_corr.csv')
+ref_dict = ref.set_index('ProteinDomain')['Corr'].to_dict()
+axes = axes.flatten()
 
-    for i, domain in enumerate(protein_domains):
-        ax = axes[i]
-        for analysis_type, linestyle, color, label in [
-            ('default', '--', 'navy', 'Default'),
-            ('sc', '-.', '#ff7f0e', 'Shuffled columns'),
-            ('scovar', '--', ke, 'Shuffled covariance')]:
+for i, domain in enumerate(protein_domains):
 
-            data_dict = load_correlation_data(base_path, domain, analysis_type)
-            if analysis_type == 'default':
-                if 'default' in data_dict:
-                    ax.plot(range(1, 13), data_dict['default'], linestyle=linestyle, color=color, label=label)
-            else:
-                means, stds = calculate_stats(data_dict)
-                ax.errorbar(range(1, 13), means, yerr=stds, linestyle=linestyle, color=color, label=label, elinewidth=1)
+    default_corr, sc_dict, scovar_dict, sr_dict = load_data(domain)
+    means1, stds1 = calculate_stats(sc_dict)
+    means2, stds2 = calculate_stats(scovar_dict)
+    means3, stds3 = calculate_stats(sr_dict)
+    ref = ref_dict[domain]
 
-        ref_corr = ref_dict.get(domain, 0)
-        ax.axhline(y=ref_corr, linestyle='-', color='r', label='Reference', linewidth=1)
-        ax.set_title(domain, fontsize=12)
+    ax = axes[i]
+    ax.plot(range(1, 13), default_corr, color='#505050', linestyle='--', label='Default')
+    ax.errorbar(range(1, 13), means1, yerr=stds1, color='#E75480',
+                linestyle='--', label='Shuffled Positions', elinewidth=1)
+    ax.errorbar(range(1, 13), means2, yerr=stds2, color='#3399FF',
+                linestyle='--', label='Shuffled Covariance', elinewidth=1)
+    ax.errorbar(range(1, 13), means3, yerr=stds3, color='limegreen',
+                linestyle='--', label='Shuffled Rows', elinewidth=1)
+    ax.axhline(y=ref, linestyle='-', color='black',
+               label='Reference', linewidth=1)
+    ax.text(11, ref + 0.05, f'{ref:.2f}', color='black', ha='center', va='bottom', fontsize=12)
 
-        if i % 5 == 0:
-            ax.set_ylabel('Rho', fontsize=10)
-        if i // 5 == 3:
-            ax.set_xlabel('Layer', fontsize=10)
+    ax.set_title(domain, fontsize=12)
+    if i >= 15:
+        ax.set_xlabel('Layer', fontsize=12)
+    if i % 5 == 0:
+        ax.set_ylabel('Rho', fontsize=12)
+    else:
+        ax.set_yticklabels([])
 
-        ax.set_xticks(list(range(1, 13, 2)))
-        ax.grid(False)
+    x_labels = list(range(1, 13, 2))
+    ax.set_xticks(x_labels)
+    ax.set_yticks(np.arange(0, 1.1, 0.2))
+    ax.tick_params(axis='x', labelsize=12)
+    ax.tick_params(axis='y', labelsize=12)
 
-    fig.tight_layout(rect=[0, 0.03, 1, 0.97])
-    handles, labels = axes[0].get_legend_handles_labels()
-    fig.legend(handles, labels, loc='lower center', ncol=4, bbox_to_anchor=(0.5, 0), fontsize=10)
-    plt.subplots_adjust(hspace=0.45, wspace=0.25)
-    plt.show()
+handles, labels = axes[0].get_legend_handles_labels()
+fig.legend(handles, labels, loc='lower center', ncol=5, bbox_to_anchor=(0.5, 0), fontsize=10)
 
-
-def load_protein_domains(file_path: str):
-    """
-    Load a list of protein domains from a file.
-    """
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        return [line.strip() for line in lines]
-
-
-if __name__ == "__main__":
-    protein_domains = load_protein_domains('./data/Pfam/protein_domain.txt')
-    base_path = 'Results'
-    reference_csv = 'score/nj_ml_corr.csv'
-    plot_correlations(protein_domains, base_path, reference_csv)
+plt.subplots_adjust(hspace=0.3, wspace=0.2)
+plt.tight_layout(rect=[0, 0.03, 1, 0.97])
+plt.show()
