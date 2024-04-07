86680efaa34eed3c5f88576f13ed9ee7e6942062
Ruyi Chen <ruyic818@gmail.com>
2024-04-07 13:25:05 +1000

    add anno

--- Plot/cmp_emb_score.py
+++ Plot/cmp_emb_score.py
@@ -11,7 +11,8 @@ from numpy.typing import ArrayLike
 
 def load_data(base_path: str, default_file_name: str):
     """
-    Loads attention score data for default, shuffled column, and shuffled covariance MSAs from a specified base path.
+    Loads attention score data for default, shuffled positions,
+    shuffled covariance and shuffled rows MSAs from a specified base path.
     """
     default_file_path = os.path.join(base_path, default_file_name)
 
@@ -24,22 +25,29 @@ def load_data(base_path: str, default_file_name: str):
     data['Layers'] = data['FileName'].str.extract('_(\d+)')[0].astype(int)
     data = data.sort_values(by=['ProteinDomain', 'Layers']).drop(['FileName'], axis=1)
     data = data.reindex(columns=['ProteinDomain', 'Layers', 'NJRFScore',
-                                 'MLRFScore', 'NJCID', 'MLCID'])
+                                 'MLRFScore', 'NJCI', 'MLCI'])
 
-    # Initialize dictionaries to store shuffled positions and covariance data
-    sc_dict = {'NJRFScore': [], 'MLRFScore': [], 'NJCID': [], 'MLCID': []}
-    scovar_dict = {'NJRFScore': [], 'MLRFScore': [], 'NJCID': [], 'MLCID': []}
+    # Initialize dictionaries to store shuffled data
+    sc_dict = {'NJRFScore': [], 'MLRFScore': [], 'NJCI': [], 'MLCI': []}
+    scovar_dict = {'NJRFScore': [], 'MLRFScore': [], 'NJCI': [], 'MLCI': []}
+    sr_dict = {'NJRFScore': [], 'MLRFScore': [], 'NJCI': [], 'MLCI': []}
 
     # Function to process shuffling files
-    def process_files(directory, is_scovar=False):
+    def process_files(directory, typ='scovar'):
         attn_file_path = os.path.join(directory, default_file_name)
+
         if os.path.exists(attn_file_path):
             attn_file_data = pd.read_csv(attn_file_path)
-            if is_scovar:
+
+            if typ == 'scovar':
                 filtered_data = attn_file_data[attn_file_data['FileName'].str.contains('scovar')]
                 target_dict = scovar_dict
+            elif typ == 'sr':
+                filtered_data = attn_file_data[attn_file_data['FileName'].str.contains('sr')]
+                target_dict = sr_dict
             else:
-                filtered_data = attn_file_data[~attn_file_data['FileName'].str.contains('scovar')]
+                filtered_data = attn_file_data[
+                    ~attn_file_data['FileName'].str.contains('scovar') & ~attn_file_data['FileName'].str.contains('sr')]
                 target_dict = sc_dict
 
             for key in target_dict.keys():
@@ -49,10 +57,11 @@ def load_data(base_path: str, default_file_name: str):
     for root, dirs, _ in os.walk(base_path):
         for dir_name in dirs:
             dir_path = os.path.join(root, dir_name)
-            process_files(dir_path, is_scovar=False)
-            process_files(dir_path, is_scovar=True)
+            process_files(dir_path, 'sc')
+            process_files(dir_path, 'scovar')
+            process_files(dir_path, 'sr')
 
-    return data, sc_dict, scovar_dict
+    return data, sc_dict, scovar_dict, sr_dict
 
 
 def merge_and_calculate_stats(dfs: DataFrame, typ: str) -> DataFrame:
@@ -91,6 +100,7 @@ def plot_heatmap(ax, data, vmin, vmax):
 
 def overlay_variance(var_data: ArrayLike, ax):
     max_variance = np.max(var_data)
+
     for i in range(var_data.shape[0]):
         for j in range(var_data.shape[1]):
             normalized_var = var_data[i, j] / max_variance
@@ -114,22 +124,22 @@ def extract_data(data: DataFrame, protein_domain: List[str], metrics: str, typ: 
 
 
 def plot_protein_domains(default_df: DataFrame, sc_dict: Dict[str, int],
-                         scovar_dict: Dict[str, int], prot_domains: List[str],
-                         metrics: str, reference_csv: str):
+                         scovar_dict: Dict[str, int], sr_dict: Dict[str, int],
+                         prot_domains: List[str], metrics: str, reference_csv: str):
     ref = pd.read_csv(reference_csv)
-    if 'CID' in metrics:
-        ref_dict = ref.set_index('ProteinDomain')['CID'].to_dict()
+    if 'CI' in metrics:
+        ref_dict = ref.set_index('ProteinDomain')['CI'].to_dict()
     else:
         ref_dict = ref.set_index('ProteinDomain')['RFScore'].to_dict()
 
-    plt.style.use('seaborn-whitegrid')
+    # plt.style.use('seaborn-whitegrid')
     plt.rcParams['font.family'] = 'sans-serif'
     plt.rcParams['font.sans-serif'] = ['arial']
 
-    fig, axs = plt.subplots(4, 5, figsize=(12, 9), sharex=False, sharey=False)
+    fig, axs = plt.subplots(4, 5, figsize=(12, 8))
     axs = axs.flatten()
 
-    typs = ['Default', 'Shuffled columns', 'Shuffled covariance']
+    typs = ['Default', 'Shuffled columns', 'Shuffled covariance', 'Shuffled rows']
     layers = list(range(1, 13))
 
     for i, protein_domain in enumerate(prot_domains):
@@ -137,6 +147,7 @@ def plot_protein_domains(default_df: DataFrame, sc_dict: Dict[str, int],
         default = extract_data(default_df, protein_domain, metrics, 'default')
         sc_avg, sc_std = extract_data(sc_dict, protein_domain, metrics, 'sc')
         scovar_avg, scovar_std = extract_data(scovar_dict, protein_domain, metrics, 'scovar')
+        sr_avg, sr_std = extract_data(sr_dict, protein_domain, metrics, 'sr')
 
         ax = axs[i]
         ax.plot(layers, default, '--', markersize=3, color='#505050', label='Default')
@@ -144,10 +155,12 @@ def plot_protein_domains(default_df: DataFrame, sc_dict: Dict[str, int],
                     label='Shuffled Positions')
         ax.errorbar(layers, scovar_avg, yerr=scovar_std, fmt='--', markersize=3, color='#3399FF',
                     label='Shuffled Covariance')
-        # Plot the reference line
-        ref_corr = ref_dict.get(protein_domain, 0)
-        ax.axhline(y=ref_corr, color='#808080', label='Reference', linewidth=1)
-        ax.text(11, ref_corr + 0.05, f'{ref_corr:.2f}', color='black', ha='center', va='bottom', fontsize=12)
+        ax.errorbar(layers, sr_avg, yerr=sr_std, fmt='--', markersize=3, color='seagreen',
+                    label='Shuffled Rows')
+
+        ref_val = ref_dict.get(protein_domain, 0)
+        ax.axhline(y=ref_val, color='#808080', label='Reference', linewidth=1)
+        ax.text(11, ref_val + 0.05, f'{ref_val:.2f}', color='black', ha='center', va='bottom', fontsize=12)
         ax.set_title(protein_domain, fontsize=14)
         ax.set_xticks(range(1, 13, 2))
         ax.set_yticks(np.arange(0, 1.1, 0.2))
@@ -159,15 +172,15 @@ def plot_protein_domains(default_df: DataFrame, sc_dict: Dict[str, int],
 
         if i % 5 == 0:
 
-            if 'CID' in metrics:
-                ax.set_ylabel('CID', fontsize=12)
+            if 'CI' in metrics:
+                ax.set_ylabel('CI Score', fontsize=12)
             else:
                 ax.set_ylabel('RF Score', fontsize=12)
         else:
             ax.set_yticklabels([])
 
     handles, labels = axs[0].get_legend_handles_labels()
-    fig.legend(handles, labels, loc='lower center', ncol=4, bbox_to_anchor=(0.5, 0), fontsize=12)
+    fig.legend(handles, labels, loc='lower center', ncol=5, bbox_to_anchor=(0.5, 0), fontsize=12)
 
     plt.subplots_adjust(hspace=0.3, wspace=0.2)
     plt.tight_layout(rect=[0, 0.03, 1, 0.97])
@@ -178,9 +191,9 @@ if __name__ == '__main__':
     base_path = 'score'
     default_file_name = 'emb_score.csv'
     reference_csv = 'score/nj_ml_score.csv'
-    metrics = 'MLRFScore'
+    metrics = 'NJCI'
     with open('./data/Pfam/protein_domain.txt', 'r') as file:
         lines = file.readlines()
     prot_domains = [line.strip() for line in lines]
-    attn_data, sc_dict, scovar_dict = load_data(base_path, default_file_name)
-    plot_protein_domains(attn_data, sc_dict, scovar_dict, prot_domains, metrics, reference_csv)
+    attn_data, sc_dict, scovar_dict, sr_dict = load_data(base_path, default_file_name)
+    plot_protein_domains(attn_data, sc_dict, scovar_dict, sr_dict, prot_domains, metrics, reference_csv)
