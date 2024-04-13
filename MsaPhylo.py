a3c701d4c3a5a4aeae7bfb9bcf6c082196133bf9
Ruyi Chen <ruyic818@gmail.com>
2024-04-13 17:07:07 +1000

    implement MsaPhylo.py

--- MsaPhylo.py
+++ MsaPhylo.py
@@ -1,4 +1,76 @@
 import argparse
+import torch
+import string
+from typing import List, Tuple
+from esm import pretrained
+from Bio import SeqIO
+from code.build_plm_tree import PlmTree
+
+
+class EmbeddingTree(PlmTree):
+    """ Class for building trees from embeddings."""
+
+    def __init__(self, msa, output_tree_path, layer=2):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.msa_fasta_file = msa
+        self.output_tree_path = output_tree_path
+        self.layer = layer
+        self.model_name = "esm_msa1b_t12_100M_UR50S"
+        self.encoding_dim, self.encoding_layer, self.max_seq_len, self.max_seq_depth = 768, 12, 1024, 1024
+
+    @staticmethod
+    def remove_insertions(sequence: str) -> str:
+        deletekeys = dict.fromkeys(string.ascii_lowercase)
+        deletekeys["."] = None
+        deletekeys["*"] = None
+        translation = str.maketrans(deletekeys)
+        return sequence.translate(translation)
+
+    def read_msa(self) -> List[Tuple[str, str]]:
+        """ Reads MSA file. """
+        return [(record.description, Extractor.remove_insertions(str(record.seq)))
+                for record in SeqIO.parse(self.msa_fasta_file, "fasta")]
+
+    def get_embedding(self):
+        """ Extracts embeddings """
+        model, alphabet = pretrained.load_model_and_alphabet(self.model_name)
+        batch_converter = alphabet.get_batch_converter()
+
+        plm_embedding = {}
+
+        model.eval()
+        msa_data = [self.read_msa()]
+        msa_labels, msa_strs, msa_tokens = batch_converter(msa_data)
+        seq_num = len(msa_labels[0])
+        seq_len = len(msa_strs[0][0])
+        if seq_len > self.max_seq_depth or seq_num > self.max_seq_len:
+            raise ValueError("It exceeds the capacity of the MSA transformer!")
+
+        with torch.no_grad():
+            out = model(msa_tokens, repr_layers=[self.layer], return_contacts=False)
+            token_representations = out["representations"][self.layer].view(seq_num, -1, self.encoding_dim)
+            # Remove the start token
+            token_representations = token_representations[:, 1:, :]
+            print(f"Finish extracting embeddings from layer {self.layer}.")
+            plm_embedding[self.layer] = token_representations
+
+        return plm_embedding
+
+    @staticmethod
+    def build_embedding_tree(self):
+        # Load the sequence names
+        prot_sequences = [record.id for record in SeqIO.parse(self.msa_fasta_file, "fasta")]
+        self.emb = self.get_embedding()
+        # Load embeddings
+        all_embeddings = torch.load(self.emb)
+
+        euc_dist = PlmTree.pairwise_euclidean_distance(all_embeddings[self.layer])
+        phylo_path = f"{self.output_tree_path}{self.prot_family}_{self.layer}.nwk"
+        tree = PlmTree.neighbor_joining(euc_dist, prot_sequences)
+
+        # Save the tree
+        with open(phylo_path, "w") as file:
+            file.write(tree)
 
 
 def main():
@@ -7,6 +79,11 @@ def main():
     parser.add_argument('--input', required=True, help='Input FASTA file path')
     parser.add_argument('--output', required=True, help='Output path to save the phylogenetic trees')
     parser.add_argument('--layer', required=False, help='Specify the layer of the MSA Transformer')
+    args = parser.parse_args()
+    msa_file = args.input
+    layer = args.layer
+    output_tree_path = args.output
+    tree = PlmTree.build_tree(msa_file, output_tree_path, layer)
 
 
 if __name__ == '__main__':
